9bb92ebaa6bebfed5254476af8c15295ed4f3005
idan gomberg <idangomberg@gmail.com>
2026-08-12 21:41:22 -0400

    remove stubs

--- CombatManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "CombatManager.h"
-
-CombatManager::CombatManager()
-{
-}
-
-CombatManager::~CombatManager()
-{
-}
--- CombatManager.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-class CombatManager {
-public:
-    CombatManager();
-    ~CombatManager();
-};
