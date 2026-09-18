0b8a8bbed7598502edf4833be022adaf7168fe83
idan gomberg <idangomberg@gmail.com>
2026-09-18 12:26:47 -0400

    chore: drop one-shot dbPath debug log (bug already diagnosed)

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -46,7 +46,6 @@ GameEngine::GameEngine(GameContext& ctx, ThreadSafeQueue<ClientInput>& input) : 
     if (const char* envBuf = std::getenv("MUD_DB_PATH"); envBuf != nullptr && *envBuf != '\0') {
         dbPath = envBuf;
     }
-    fprintf(stderr, "DEBUG: GE 0 (dbPath=%s)\n", dbPath.c_str()); fflush(stderr);
 
     fprintf(stderr, "DEBUG: GE 1\n"); fflush(stderr);
     world = new World();
